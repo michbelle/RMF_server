60c1c3b2f4a5302895d658ded851a625373ba729
Michele <michele.belletti@elettra.eu>
2025-09-30 12:56:37 +0000

    fix root docker

--- src/rmf_server_elettra/ign_world/setup_ign.sh
+++ src/rmf_server_elettra/ign_world/setup_ign.sh
@@ -1,1 +1,1 @@
-export GZ_SIM_RESOURCE_PATH=/root/ff_ws/src/myTesiCode/rmf_code/ign_world/elettra/modelOUT/
+export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:/openRMF_ws/src/rmf_server/src/rmf_server_elettra/ign_world/elettra/modelOUT/
